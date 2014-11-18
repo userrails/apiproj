0a7dcce38f9b84c45595dbf83d4c37ab2f228f0e
rails <rails@rails>
2014-11-18 21:46:55 +0530

    update user to save the authentiction token code

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+ after_create :save_authentication_token
+
+ def save_authentication_token
+ 	self.update(:authentication_token => Devise.friendly_token[0,10])
+ end
 end
